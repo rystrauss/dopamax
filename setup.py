17a39742fbf51d341a993d89e9d85f05a40e7b0b
Ryan Strauss <ryanrstrauss@icloud.com>
2023-12-29 15:41:19 -0500

    Update package workflows and setup.py

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",

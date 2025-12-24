a6bb074abccbd78bfa991f1b5cdfc5ca67a0179d
Ryan Strauss <ryanrstrauss@icloud.com>
2025-12-24 15:07:26 -0500

    Add missing env imports in __init__

--- src/dopamax/environments/__init__.py
+++ src/dopamax/environments/__init__.py
@@ -1,4 +1,9 @@
 from .brax.ant import Ant
 from .brax.half_cheetah import HalfCheetah
 from .brax.inverted_pendulum import InvertedPendulum
+from .gymnax import GymnaxEnvironment
+from .pgx.chess import Chess
+from .pgx.connect_four import ConnectFour
+from .pgx.go import Go9x9, Go19x19
+from .pgx.tic_tac_toe import TicTacToe
 from .utils import make_env
